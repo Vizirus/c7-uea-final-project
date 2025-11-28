ba71860a738b54356b9fd65289fb7414605a6257
artem <denloken@gmail.com>
2025-11-28 17:02:14 +0000

    First commit. Added basic structure of the project. Node server have been utilised as well.

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("page_code"));
+app.get("/", (req, res) => {
+    res.sendFile('index.html', (err)=>{
+    if (err)
+        console.log(err);
+    });
+});
+app.listen(port, () => {
+    console.log(`myapp is listening on port ${port}!`);
+});
+console.log("Server started on port " + port);
